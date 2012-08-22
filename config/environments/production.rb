d2624523c1930b1450697b185ab03a14cd27ae6e
nicholashibberd <nicholashibberd@gmail.com>
2012-08-22 18:18:43 +0100

    adding images to the precompilation path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,6 @@ DefaultSite::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
@@ -45,6 +44,7 @@ DefaultSite::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
   config.assets.precompile += ['admin.js']
+  config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
